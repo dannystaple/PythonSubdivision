909751fb278d8e958f01cf3f87d3dc778254e759
Danny Staple <danny@orionrobots.co.uk>
2014-01-26 22:11:00 +0000

    Node structure ditched for simpler list of lists style system. Less code. However - incompatible with the old files.

--- pygame_subdiv.py
+++ pygame_subdiv.py
@@ -5,50 +5,35 @@ black = 0, 0, 0
 white = 0xff, 0xff, 0xff
 
 
-class Node:
-    def __init__(self):
-        self._subNodes = None
-
-    def subDivide(self):
-        self._subNodes = [Node(), Node(), Node(), Node()]
-
-    def subNodes(self):
-        return self._subNodes
-
-    def clearSubNodes(self):
-        self._subNodes = None
+def subdivide():
+    return [[], [], [], []]
 
 
-class Tree:
-    def __init__(self):
-        self._root = Node()
-
-    def getRoot(self):
-        """Get the tree root"""
-        return self._root
-
-
-def test_tree(root):
-    root.subDivide()
-    sn0 = root.subNodes()[0]
-    sn0.subDivide()
-    sn1 = sn0.subNodes()[1]
-    sn1.subDivide()
-    sn2 = sn1.subNodes()[0]
-    sn2.subDivide()
+def test_tree():
+    tree = subdivide()
+    tree[0] = subdivide()
+    tree[0][1] = subdivide()
+    tree[0][1][0] = subdivide()
+    return tree
 
 
 class Game:
+    _tree = []
+    _lastChanged = None
+    _squares = None
+    _file_name = None
+    _screen = None
+
     def __init__(self):
         self._reset()
         self._width, self._height = 768, 768
-        test_tree(self._tree.getRoot())
+        self._tree = test_tree()
 
     def _reset(self):
         self._lastChanged = None
-        self._tree = Tree()
+        self._tree = []
         self._squares = None
-        self._fileName = "test.tree"
+        self._file_name = "test.tree"
 
     def run(self):
         self._init_screen()
@@ -57,8 +42,10 @@ class Game:
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT: sys.exit()
-                if event.type == pygame.MOUSEBUTTONDOWN: self.handle_click_at(pygame.mouse.get_pos(), event.button)
-                if event.type == pygame.KEYDOWN: self.handle_key(event.key)
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    self.handle_click_at(pygame.mouse.get_pos(), event.button)
+                if event.type == pygame.KEYDOWN:
+                    self.handle_key(event.key)
             self._update_display()
             time.sleep(0.1)
 
@@ -72,19 +59,18 @@ class Game:
         """Convert a tree of nodes to squares.
         Each node will result in 4 subdivisions."""
 
-        def _get_squares_for_node(node, x, y, width, height):
+        def _get_squares_for_node(sub_nodes, x, y, width, height):
             hwidth = width / 2
             hheight = height / 2
-            subNodes = node.subNodes()
-            if hwidth >= 1 and hheight >= 1 and subNodes:
-                return (_get_squares_for_node(subNodes[0], x, y, hwidth, hheight) +
-                        _get_squares_for_node(subNodes[1], x + hwidth, y, hwidth, hheight) +
-                        _get_squares_for_node(subNodes[2], x, y + hheight, hwidth, hheight) +
-                        _get_squares_for_node(subNodes[3], x + hwidth, y + hheight, hwidth, hheight))
+            if hwidth >= 1 and hheight >= 1 and sub_nodes:
+                return (_get_squares_for_node(sub_nodes[0], x, y, hwidth, hheight) +
+                        _get_squares_for_node(sub_nodes[1], x + hwidth, y, hwidth, hheight) +
+                        _get_squares_for_node(sub_nodes[2], x, y + hheight, hwidth, hheight) +
+                        _get_squares_for_node(sub_nodes[3], x + hwidth, y + hheight, hwidth, hheight))
             else:
                 return [[x, y, width, height]]
 
-        self._squares = _get_squares_for_node(self._tree.getRoot(), 0, 0, self._width, self._height)
+        self._squares = _get_squares_for_node(self._tree, 0, 0, self._width, self._height)
 
     def _update_display(self):
         self._screen.fill(white)
@@ -95,20 +81,20 @@ class Game:
 
     def load_file(self, filename):
         self._lastChanged = None
-        self._fileName = filename
+        self._file_name = filename
         with open(filename) as fp:
             self._tree = pickle.load(fp)
         self._tree_to_squares()
-        print "Loaded file %s" % self._fileName
+        print "Loaded file %s" % self._file_name
 
     def save_file(self):
-        print "Writing file %s" % self._fileName
-        with open(self._fileName, "w") as fp:
+        print "Writing file %s" % self._file_name
+        with open(self._file_name, "w") as fp:
             pickle.dump(self._tree, fp)
 
     def handle_key(self, key):
         """Handle keypresses"""
-        key = key & 0xff
+        key &= 0xff
         print "Handling key '" + chr(key) + "'"
         if chr(key) == 'q':
             sys.exit()
@@ -128,38 +114,38 @@ class Game:
         """Given a click at an x,y tuple (pos) it should subdivide the node it landed on"""
         x, y = pos
         node = None
-        newNode = self._tree.getRoot()
+        new_node = self._tree
         sx = 0
         sy = 0
         w = self._width
         h = self._height
-        while newNode != node and w > 1 and h > 1 and newNode.subNodes():
-            node = newNode
+        while new_node != node and w > 1 and h > 1 and new_node:
+            node = new_node
             w /= 2
             h /= 2
             if x < (sx + w):
                 if y < (sy + h):
                     print "Using first sn"
-                    newNode = node.subNodes()[0]
+                    new_node = node[0]
                 else:
                     print "Using 3rd sn"
                     sy += h
-                    newNode = node.subNodes()[2]
+                    new_node = node[2]
             else:
                 sx += w
                 if y < (sy + h):
                     print "Using 2nd sn"
-                    newNode = node.subNodes()[1]
+                    new_node = node[1]
                 else:
                     print "Using 4th sn"
                     sy += h
-                    newNode = node.subNodes()[3]
+                    new_node = node[3]
         if button == 1:
-            newNode.subDivide()
-            self._lastChanged = newNode
+            new_node.extend(subdivide())
+            self._lastChanged = new_node
             self._tree_to_squares()
-        if button == 2:
-            node.clearSubNodes()
+        if button == 3:
+            node[:] = []
             self._tree_to_squares()
 
 
